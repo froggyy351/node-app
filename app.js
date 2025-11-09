7ea435ed056c608b7aa9933486536b87b5e61a76
Koichi Sakogawa <kouichi19980817@gmail.com>
2025-11-09 20:11:03 +0900

    update in 2025/11/09

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+var data = {
+    'Taro': '09-999-999',
+    'Hanako': '080-888-888',
+    'Sachiko': '070-777-777',
+    'Ichiro': '060-666-666',
+    'aga': '060-666-666',
+    'otake': '060-666-666',
+    'watashi': '060-666-666',
+    'haha': '060-666-666',
+    'chichi': '060-666-666',
+    'fsdafa': '060-666-666',
+};
+
+
+var data2 = {
+    'Taro': ['Taro@yamada', '09-999-999', 'Tokyo'],
+    'Hanako': ['hanako@flower', '080-888-888', 'Yokohama'],
+    'Sachi': ['Sachi@happy', '070-777-777', 'Nagoya'],
+    'Ichiro': ['ichi@baseball', '060-666-666', 'USA'],
+}
+
 const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
@@ -47,7 +68,9 @@ function response_index(request, response) {
     var content = ejs.render(index_page, {
         title: "index",
         content: msg,
-    });
+        data: data,
+        filename: 'data_item'
+    }); 
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(content);
     response.end();
@@ -56,37 +79,47 @@ function response_index(request, response) {
 // otherアクセス処理
 function response_other(request, response) {
     var msg = "これはOtherページです。";
+    var content = ejs.render(other_page, {
+        title: "Other",
+        content: msg,
+        data: data2,
+        filename: 'data_item'
+    });
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(content);
+    response.end();      
 
-    //POSTアクセス時の処理
-    if (request.method == 'POST'){
-        var body = '';
 
-        //データ受信のイベント処理
-        request.on('data', (data) => {
-            body += data;
-        });
+    // //POSTアクセス時の処理
+    // if (request.method == 'POST'){
+    //     var body = '';
 
-        //データ受信終了のイベント処理
-        request.on('end', () => {
-            var post_data = qs.parse(body); //データのパース
-            msg += 'あなたは、「' + post_data.msg + '」と書きました。';
-            var content = ejs.render(other_page, {
-                title: "Other",
-                content: msg,
-            });
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.write(content);
-            response.end();        
-        })
+    //     //データ受信のイベント処理
+    //     request.on('data', (data) => {
+    //         body += data;
+    //     });
 
-    } else {
-        var msg = "ページがありません";
-        var content = ejs.render(other_page, {
-            title: "Other",
-            content: msg,
-        });
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.write(content);
-        response.end(); 
-    }
+    //     //データ受信終了のイベント処理
+    //     request.on('end', () => {
+    //         var post_data = qs.parse(body); //データのパース
+    //         msg += 'あなたは、「' + post_data.msg + '」と書きました。';
+    //         var content = ejs.render(other_page, {
+    //             title: "Other",
+    //             content: msg,
+    //         });
+    //         response.writeHead(200, { 'Content-Type': 'text/html' });
+    //         response.write(content);
+    //         response.end();        
+    //     })
+
+    // } else {
+    //     var msg = "ページがありません";
+    //     var content = ejs.render(other_page, {
+    //         title: "Other",
+    //         content: msg,
+    //     });
+    //     response.writeHead(200, { 'Content-Type': 'text/html' });
+    //     response.write(content);
+    //     response.end(); 
+    // }
 } 
