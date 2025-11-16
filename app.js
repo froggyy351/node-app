88b163886481641f946a92da3f2fb029c8e34070
Koichi Sakogawa <kouichi19980817@gmail.com>
2025-11-16 17:28:01 +0900

    add cookie

--- app.js
+++ app.js
@@ -78,6 +78,8 @@ function response_index(request, response) {
         //データ受信終了のイベント処理
         request.on('end', () => {
             data = qs.parse(body); //データのパース
+            //クッキーの保存
+            setCookie('msg', data.msg, response)
             write_index(request, response);    
         });
     } else {
@@ -88,14 +90,35 @@ function response_index(request, response) {
 //indexの表示作成
 function write_index(request, response){
     var msg = "※伝言を表示します。";
+    var cookie_data = getCookie('msg', request);
     var content = ejs.render(index_page, {
         title: "index",
         content: msg,
         data: data,
+        cookie_data: cookie_data,
     });
     response.writeHead(200, { 'Content-type': 'text/html'});
     response.write(content);
     response.end();
+}
+
+//　クッキーの値を設定
+function setCookie(key, value, response){
+    var cookie = escape(value);
+    response.setHeader('Set-Cookie', [key + '=' + cookie]);
+}
+
+//　クッキーの値を取得
+function getCookie(key, request){
+    var cookie_data = request.headers.cookie != undefined ? request.headers.cookie : '';
+    var data = cookie_data.split(';');
+    for(var i in data) {
+        if (data[i].trim().startsWith(key + '=')) {
+            var result = data[i].trim().substring(key.length + 1);
+            return unescape(result);
+        }
+    }
+    return '';
 }
 
 // otherアクセス処理
